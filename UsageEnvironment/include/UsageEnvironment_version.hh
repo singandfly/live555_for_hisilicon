f77f945e4e2f60a508c364c2b7d211dc6c683883
zhifeng.cheng <zhifeng.cheng@hobot.cc>
2018-02-19 01:19:17 +0800

    initial version for hi3516, hi3519, hi3559

---
+++ UsageEnvironment/include/UsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "UsageEnvironment" library
+// Copyright (c) 1996-2017 Live Networks, Inc.  All rights reserved.
+
+#ifndef _USAGEENVIRONMENT_VERSION_HH
+#define _USAGEENVIRONMENT_VERSION_HH
+
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2017.04.26"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1493164800
+
+#endif
